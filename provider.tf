3bb62e010bc08254d775ca7337649c9c40cac469
Jangdochi <hunjoon0501@naver.com>
2026-04-30 09:54:13 +0900

    제거

--- provider.tf
+++ provider.tf
@@ -5,14 +5,6 @@ terraform {
       version = "~> 5.0"
     }
   }
-
-  backend "s3" {
-    bucket         = "jhjnew-bucket"
-    key            = "training/lab06/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  #  dynamodb_table = "terraform-lock"
-  }
 }
 
 provider "aws" {
