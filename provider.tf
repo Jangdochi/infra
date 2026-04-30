28d3649e49cff481dd9a120e45157a7acd8d693f
Jangdochi <hunjoon0501@naver.com>
2026-04-30 09:51:58 +0900

    #  dynamodb_table = terraform-lock

--- provider.tf
+++ provider.tf
@@ -11,7 +11,7 @@ terraform {
     key            = "training/lab06/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-lock"
+  #  dynamodb_table = "terraform-lock"
   }
 }
 
