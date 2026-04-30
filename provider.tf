d1c0a025acebe19388d5281d6726cb1046dde90d
Jangdochi <hunjoon0501@naver.com>
2026-04-30 09:35:41 +0900

    first commit

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "jhjnew-bucket"
+    key            = "training/lab06/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
